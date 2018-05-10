0195e81a728a394c1e6258168108e7c61b8efe04
Anwesh43 <anweshthecool0@gmail.com>
2018-05-11 03:35:33 +0530

    adding View in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinsliderpointview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsliderpointview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinsliderpointview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sliderpointview.SliderPointView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SliderPointView.create(this)
     }
 }
