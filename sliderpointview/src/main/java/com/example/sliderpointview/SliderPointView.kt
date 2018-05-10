653498b1e428a67ac921c8763ed1a7ab54a1ef13
Anwesh43 <anweshthecool0@gmail.com>
2018-05-11 02:55:41 +0530

    adding SliderPointView class

--- sliderpointview/src/main/java/com/example/sliderpointview/SliderPointView.kt
+++ sliderpointview/src/main/java/com/example/sliderpointview/SliderPointView.kt
@@ -3,3 +3,27 @@ package com.example.sliderpointview
 /**
  * Created by anweshmishra on 11/05/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.graphics.*
+import android.view.MotionEvent
+
+class SliderPointView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
